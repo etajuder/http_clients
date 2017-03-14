b368da87d6d24db252e4ce59ab207c0fc556119b
wapjude <wapjude@gmail.com>
2017-03-14 15:10:59 +0000

    Add SMS Sender App

--- app/index.js
+++ app/index.js
@@ -10,61 +10,61 @@
 
 if (!String.prototype.format) {
 
-  String.prototype.format = function() {
+            String.prototype.format = function() {
 
-    var args = arguments;
+    		var args = arguments;
 
-    return this.replace(/{(\d+)}/g, function(match, number) { 
+    		return this.replace(/{(\d+)}/g, function(match, number) { 
 
-      return typeof args[number] != 'undefined'
-        ? args[number]
-        : match
-      ;
+     		 return typeof args[number] != 'undefined' ? args[number]: match
 
-    });
+              ;
+
+    		});
 
   };
 
 }
 
- prompt.start();
- 
-console.log(chalk.blue("Welcome to Multiple Sms Sender here you can send SMS to Many users using this wonderful command line App"));
+ 	prompt.start();
 
-console.log(chalk.green("Please Select your Operation"));
+	console.log(chalk.blue("Welcome to Multiple Sms Sender here you can send SMS to Many users using this wonderful command line App"));
 
-console.log(chalk.yellow("1: Check your Account Balance"));
+	console.log(chalk.green("Please Select your Operation"));
 
-console.log(chalk.cyan("2: Send Sms"));
+	console.log(chalk.yellow("1: Check your Account Balance"));
 
-prompt.get(["operation"],function(err,result){
+	console.log(chalk.cyan("2: Send Sms"));
 
-    switch(result.operation){
+	prompt.get(["operation"],function(err,result){
 
-    	case "1":
+   			 switch(result.operation){
 
-        var status = new Spinner('Getting your SMS Account Balance, please wait...');
+    				case "1":
 
-           status.start();
+        			 var status = new Spinner('Getting your SMS Account Balance, please wait...');
 
-            var username = "wapjuder";
+         			  status.start();
 
-            var password = "password";
+          			  var username = "wapjuder";
 
-           var url = "http://smsmobile24.com/components/com_spc/smsapi.php?username={0}&password={1}&balance=true&".format(username,password);
+           			 var password = "password";
 
-           request(url,function(error, response, body){
+          			 var url = "http://smsmobile24.com/components/com_spc/smsapi.php?username={0}&password={1}&balance=true&".format(username,password);
 
-           	if(response.statusCode === 200){
+          			 request(url,function(error, response, body){
 
-           		status.stop();
 
-           		console.log(chalk.green("Your Account Balance is "+body))
+           			if(response.statusCode === 200){
+
+           						status.stop();
+
+           						console.log(chalk.green("Your Account Balance is "+body))
            		
-           	}else{
+           			}else{
 
-           		console.log(chalk.red("Error occur please try again"));
-           	}
+           						console.log(chalk.red("Error occur please try again"));
+           			}
            });
 
 
@@ -72,47 +72,50 @@ prompt.get(["operation"],function(err,result){
 
     	case "2":
     	
-    	console.log(chalk.white("Please Enter the numbers to send to."));
+    				console.log(chalk.white("Please Enter the numbers to send to."));
     	
-    	console.log(chalk.white("Enter the message to be sent"));
+    				console.log(chalk.white("Enter the message to be sent"));
 
-    	console.log(chalk.white("Enter the Sender Name"));
+    				console.log(chalk.white("Enter the Sender Name"));
  
     	
-    	prompt.get(["numbers","message","sender"],function(err,result){
+    				prompt.get(["numbers","message","sender"],function(err,result){
 
-    		var username = "wapjuder";
+    				var username = "wapjuder";
 
-            var password = "password";
+            		var password = "password";
 
-    		var url = "http://smsmobile24.com/components/com_spc/smsapi.php?username={0}&password={1}&sender={2}&recipient={3}&message={4}";
+    				var url = "http://smsmobile24.com/components/com_spc/smsapi.php?username={0}&password={1}&sender={2}&recipient={3}&message={4}";
 
-    		url =url.format(username,password,result.sender,result.numbers,result.message);
+    				url =url.format(username,password,result.sender,result.numbers,result.message);
 
-           var status = new Spinner('Sending Message to '+ result.numbers);
+           			var status = new Spinner('Sending Message to '+ result.numbers);
 
-           status.start();
+           			status.start();
 
-    		request(url,function(error, response, body){
+    				request(url,function(error, response, body){
 
-    			if(response.statusCode === 200){
+    						if(response.statusCode === 200){
 
-    				status.stop();
+    									status.stop();
 
-    				console.log(chalk.green("Your Message have been sent to "+request.numbers));
+    									console.log(chalk.green("Your Message have been sent to "+request.numbers));
 
-    			}else{
+    						}else{
 
-    				console.log(chalk.red("Error Sending message"));
+    									console.log(chalk.red("Error Sending message"));
 
-    			}
+    						}
+    				});
+
     		});
 
-    	});
-
     	break;
+
     	default:
-           console.log(chalk.red("Invalid Operation specified"));
+
+          		 console.log(chalk.red("Invalid Operation specified"));
+
     	break;
     }
 });
